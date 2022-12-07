c52a7d490e1e055ec97315097f60f40375e09fc5
Alpha05Gamma <551den05@gmail.com>
2022-12-07 14:39:12 +0300

    first

---
+++ Obcerver.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FileObcerver
+{
+    static class Obcerver
+    {
+        static Process process = new Process();
+        public static List<int> Obcerve ()
+        {
+            Console.SetCursorPosition(0, 2);
+            List<int> list = new List<int>();
+
+            foreach(Process process in Process.GetProcesses())
+            {
+                
+                Console.WriteLine($"  ID: {process.Id}  Name: {process.ProcessName}");
+                list.Add(process.Id);
+               
+            }
+            Console.SetCursorPosition(0, 0);
+            return list;
+        }
+        public static void Run(string name)
+        {
+            try
+            {
+                if (File.Exists(name)) //Если файл, то запуск через PowerShell
+                {
+                    Process.Start(new ProcessStartInfo { FileName = name, UseShellExecute = true });
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(exception.Message);
+            }
+        }
+        public static void ObcerveInfo(int indexId)
+        {
+            
+
+            foreach (Process process in Process.GetProcesses())
+            {
+                if (process.Id == indexId)
+                {
+                    Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 2, 6);
+                    Console.Write($"  Physical memory usage     : {process.WorkingSet64}");
+                    Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 2, 7);
+                    Console.Write($"  Base priority             : {process.BasePriority}");
+                    Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 2, 8);
+                    Console.Write($"  Paged system memory size  : {process.PagedSystemMemorySize64} Bytes");
+                    Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 2, 9);
+                    Console.Write($"  Paged memory size         : {process.PagedMemorySize64} Bytes");
+                }
+                
+
+            }
+            Console.SetCursorPosition(0, 0);
+        }
+
+        public static void Obliterator(int indexId)
+        {
+            try
+            {
+                foreach (Process process in Process.GetProcesses())
+                {
+                    if (process.Id == indexId)
+                    {
+                       process.Kill();
+                    }
+
+
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(exception.Message);
+            }
+        }
+    }
+}
---
+++ UI.cs
@@ -0,0 +1,117 @@
+﻿using Microsoft.VisualBasic;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace FileObcerver
+{
+    internal class UI
+    {
+        private int index = 2; //Номер положения указателя
+        List<int> positions = new List<int>(); //Список элементов
+        
+
+
+        keyboard keyboard = new keyboard(); //Создается экземпляр модели клавиатуры
+        
+        public string[] description= new string[3]{"F1 - создать процесс", "Enter - информация о процессе", "Space - остановить"};
+        //И описание фунционала меню
+        public void drawGrid() //Отрисовка сетки интерфейса
+        {
+            Console.Clear();
+            Console.CursorVisible = false;
+            Console.SetCursorPosition(0, 1);
+            for(int i = 0; i < Console.BufferWidth; i++)
+            {
+                Console.Write("-");
+            }
+            for (int i = 2; i < 17 ; i++)
+            {
+                Console.SetCursorPosition(Console.BufferWidth / 5 * 3, i);
+                Console.Write("|");
+            }
+            Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 1, 5);
+            for (int i = Console.BufferWidth / 5 * 3 + 1; i < Console.BufferWidth; i++)
+            {
+                Console.Write("-");
+            }
+            Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 1, 2);
+            for(int i =0; i<description.Length; i++)
+            {
+                Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 1, 2+i);
+                Console.WriteLine(description[i]);
+            }
+            
+        }
+
+        public void menu()
+        {
+            keyboard.OnChange += Detector; //Подписываемся на событие с нажатием клавиши
+            new Thread(keyboard.keyControl).Start(); //Запускаем уловитель события в поток
+            drawGrid(); //Вывод интерфейса
+            Console.SetBufferSize(Console.BufferWidth, 20000);
+            positions = Obcerver.Obcerve();
+            Console.SetCursorPosition(0, 1);
+
+        }
+
+        public void Detector(ConsoleKey consoleKey) //Обработчик нажатий
+        {
+
+            switch (consoleKey)
+            {
+                case ConsoleKey.UpArrow://Перемещение указателя на 1 вверх
+                    Arrow(-1);
+                    break;
+                case ConsoleKey.DownArrow://Перемещение указателя на 1 вниз
+                    Arrow(1);
+                    break;
+                case ConsoleKey.Enter: //информация 
+                    Obcerver.ObcerveInfo(positions[index - 2]);
+                    break;
+                case ConsoleKey.Spacebar: //остановить
+                    Console.SetCursorPosition(0, 0);
+                    for (int i = 0; i < 100; i++)
+                    {
+                        Console.WriteLine(" ");
+                        Console.SetCursorPosition(0 + i, 0);
+                    }
+                    Obcerver.Obliterator(positions[index - 2]);
+                    positions = Obcerver.Obcerve();
+                    break;
+                case ConsoleKey.F1:
+                    Console.CursorVisible = true;
+                    Console.SetCursorPosition(Console.BufferWidth / 5 * 3 + 2, 6);
+                    string name = Console.ReadLine();//Считываем название файла
+                    Obcerver.Run(name);
+                    Console.CursorVisible = false;
+                    break;
+
+            }
+
+        }
+
+        private void Arrow(int direction)
+        {
+            Console.SetCursorPosition(1, index);
+            Console.Write(" "); //удаляем старый индикатор
+            index = index + direction;
+            if(index<positions.Count && index >= 1) //Проверяем новый на выход за предел значений
+            {
+                Console.SetCursorPosition(1, index);
+                Console.Write(">"); //рисуем
+            }
+            else
+            {
+                index = 1; //индикатор в положение по умолчанию
+                
+            }
+        }
+
+    }
+}
